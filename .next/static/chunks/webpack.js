13bc85664cd2cdd6cb436cb3e4f585388fe8c35b
satyendra1010 <satyendra1010@gmail.com>
2023-03-08 21:31:59 -0700

    Updated Portfolio.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b981ba92a05b40f3"; }
+/******/ 		__webpack_require__.h = function() { return "16958c7f6b2b6cf7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.551220d6924eb54b.hot-update.js
+++ .next/static/webpack/webpack.551220d6924eb54b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b981ba92a05b40f3"; }
+/******/ 	__webpack_require__.h = function() { return "7a33cfe07c522d18"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
